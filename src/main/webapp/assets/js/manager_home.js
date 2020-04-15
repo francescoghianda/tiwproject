3cef59d1fcffb929bb0586c8721ae1ff825697ab
francescoghianda <francy9661@gmail.com>
2020-04-15 04:04:29 +0200

    .

--- src/main/webapp/assets/js/manager_home.js
+++ src/main/webapp/assets/js/manager_home.js
@@ -1,5 +1,15 @@
 $(() =>
 {
+    $('#add-campaign-btn').on('click', () =>
+    {
+        $('.new-campaign-form').addClass('visible');
+    })
+
+    $('#close-new-campaign').on('click', () =>
+    {
+        $('.new-campaign-form').removeClass('visible');
+    })
+
     $('#formCampaign').on('click', function ()
     {
         console.log('click');
