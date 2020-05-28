746aef15f38f917614111350d448e795ab189941
francescoghianda <francy9661@gmail.com>
2020-05-28 15:15:35 +0200

    changed map provider

--- src/main/webapp/assets/js/add-annotation.js
+++ src/main/webapp/assets/js/add-annotation.js
@@ -10,9 +10,14 @@ $(() =>
 
     let map = L.map('map').setView([lat, lon], zoom);
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    /*L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 18,
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);*/
+
+    L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+        maxZoom: 18,
+        attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     }).addTo(map);
 
     $(".map-target-btn").on('click', function () {
